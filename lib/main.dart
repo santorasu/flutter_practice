31a2f9241cc71f59d07b2aa8e07e9c8b3035a269
Santo Rasu <santorasu09.com>
2025-01-16 00:46:19 +0600

    This is button practice

--- lib/main.dart
+++ lib/main.dart
@@ -14,86 +14,29 @@ home: SafeArea(
       ),),
       centerTitle: true,
     ),
-    body: Container(
-
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text("R1",style: TextStyle(fontSize: 25),),
-                  Text("R2",style: TextStyle(fontSize: 25),),
-                  Text("R3",style: TextStyle(fontSize: 25),),
-                  Text("R4",style: TextStyle(fontSize: 25),),
-                  Text("R5",style: TextStyle(fontSize: 25),),
-
-                  Text("R1",style: TextStyle(fontSize: 25),),
-                  Text("R2",style: TextStyle(fontSize: 25),),
-                  Text("R3",style: TextStyle(fontSize: 25),),
-                  Text("R4",style: TextStyle(fontSize: 25),),
-                  Text("R5",style: TextStyle(fontSize: 25),),
-
-                  Text("R1",style: TextStyle(fontSize: 25),),
-                  Text("R2",style: TextStyle(fontSize: 25),),
-                  Text("R3",style: TextStyle(fontSize: 25),),
-                  Text("R4",style: TextStyle(fontSize: 25),),
-                  Text("R5",style: TextStyle(fontSize: 25),),
-                ],
-              ),
-            ),
-        
-            Text("A",style: TextStyle(fontSize: 25),),
-            Text("B",style: TextStyle(fontSize: 25),),
-            Text("C",style: TextStyle(fontSize: 25),),
-            Text("D",style: TextStyle(fontSize: 25),),
-            Text("E",style: TextStyle(fontSize: 25),),
-
-            Text("A",style: TextStyle(fontSize: 25),),
-            Text("B",style: TextStyle(fontSize: 25),),
-            Text("C",style: TextStyle(fontSize: 25),),
-            Text("D",style: TextStyle(fontSize: 25),),
-            Text("E",style: TextStyle(fontSize: 25),),
-
-            Text("A",style: TextStyle(fontSize: 25),),
-            Text("B",style: TextStyle(fontSize: 25),),
-            Text("C",style: TextStyle(fontSize: 25),),
-            Text("D",style: TextStyle(fontSize: 25),),
-            Text("E",style: TextStyle(fontSize: 25),),
-
-            Text("A",style: TextStyle(fontSize: 25),),
-            Text("B",style: TextStyle(fontSize: 25),),
-            Text("C",style: TextStyle(fontSize: 25),),
-            Text("D",style: TextStyle(fontSize: 25),),
-            Text("E",style: TextStyle(fontSize: 25),),
-            Text("A",style: TextStyle(fontSize: 25),),
-            Text("B",style: TextStyle(fontSize: 25),),
-            Text("C",style: TextStyle(fontSize: 25),),
-            Text("D",style: TextStyle(fontSize: 25),),
-            Text("E",style: TextStyle(fontSize: 25),),
-            Text("A",style: TextStyle(fontSize: 25),),
-            Text("B",style: TextStyle(fontSize: 25),),
-            Text("C",style: TextStyle(fontSize: 25),),
-            Text("D",style: TextStyle(fontSize: 25),),
-            Text("E",style: TextStyle(fontSize: 25),),
-           ElevatedButton(
-               style: ElevatedButton.styleFrom(
-                 backgroundColor: Colors.deepOrange,
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(30)
-                 )
-               )
-               ,onPressed: (){
-             print("I am a button");
-           },
-               child: Text("Click"))
-          ],
+    body: Column(
+      children: [
+        Center(
+          child: ElevatedButton(onPressed: (){
+            print("I am Elevated Button");
+          }, child: Row(
+            children: [
+              Icon(Icons.add,size: 50,color: Colors.green),
+              Text("Button",
+              style: TextStyle(fontSize: 25,
+              color:Colors.green),),
+            ],
+          )),
         ),
-      ),
+        Icon(Icons.person,size: 50,color: Colors.green,)
+        ,
+        TextButton(onPressed: (){
+          print("Im Text Button");
+        }, child: Text("Click Me",style: TextStyle(
+          fontSize: 25,
+          color: Colors.red
+        ),))
+      ],
     )
   ),
 )
